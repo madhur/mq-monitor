15c859ff9dfa0684164367efedaa585820a6c893
Madhur <ahuja.madhur@gmail.com>
2015-04-26 22:18:18 +0530

    updated

---
+++ app/src/main/java/in/co/madhur/rabbitmonitor/App.java
@@ -0,0 +1,9 @@
+package in.co.madhur.rabbitmonitor;
+
+import android.app.Application;
+
+/**
+ * Created by madhur on 4/26/15.
+ */
+public class App extends Application {
+}
---
+++ app/src/main/java/in/co/madhur/rabbitmonitor/MainActivity.java
@@ -0,0 +1,12 @@
+package in.co.madhur.rabbitmonitor;
+
+import android.app.Activity;
+
+/**
+ * Created by madhur on 4/26/15.
+ */
+public class MainActivity extends Activity {
+
+
+
+}
