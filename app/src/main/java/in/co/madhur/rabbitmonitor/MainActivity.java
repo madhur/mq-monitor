1f12f4689530e7a64189edf8da741f6c8e5a9d71
Madhur <ahuja.madhur@gmail.com>
2015-05-13 16:24:07 +0530

    added some more files

--- app/src/main/java/in/co/madhur/rabbitmonitor/MainActivity.java
+++ app/src/main/java/in/co/madhur/rabbitmonitor/MainActivity.java
@@ -1,6 +1,7 @@
 package in.co.madhur.rabbitmonitor;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 /**
  * Created by madhur on 4/26/15.
@@ -8,5 +9,10 @@ import android.app.Activity;
 public class MainActivity extends Activity {
 
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
+        setContentView(R.layout.main_layout);
+    }
 }
